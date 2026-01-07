d45c912f34d9efabd2165be778f09f0229b6e75e
Luzzi Charly <luzzi.charly@gmail.com>
2026-01-07 22:13:50 +0100

    Removes duplicate background
    html tag

--- app.vue
+++ app.vue
@@ -13,7 +13,7 @@
 html, body, #__nuxt {
     height: auto; /* Allow height to expand with content */
     min-height: 100vh; /* Ensure it covers at least the viewport height */
-    background: #222;
+    /* background: #222; */
     color: white;
 }
 </style>
