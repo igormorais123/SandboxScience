ae5ee66ae58eabc9f8a56bcb132f78b9edb14ed7
Luzzi Charly <luzzi.charly@gmail.com>
2026-03-13 15:45:07 +0100

    Refine Support Btn
    Polishes the donation button's appearance by adjusting the outline style and padding.
    
    - Replaces ring utilities with borders
    - Slightly reduces padding for a tighter look

--- components/particle-life/ParticleLifeGpu.vue
+++ components/particle-life/ParticleLifeGpu.vue
@@ -315,7 +315,7 @@
         </div>
         <section fixed z-10 bottom-2 right-2 flex items-end>
             <button type="button" name="Donate" aria-label="Donate" title="Donate" @click="openDonationModal()"
-                    class="donation-glow group relative flex items-center mr-2 p-1.5 rounded-full backdrop-blur-sm transition-all duration-300 bg-rose-600/80 hover:bg-rose-500/90 ring-1 ring-rose-400/50 hover:ring-rose-300/70 text-white">
+                    class="donation-glow group relative flex items-center mr-2 p-1.2 rounded-full backdrop-blur-sm transition-all duration-300 bg-rose-600/80 hover:bg-rose-500/90 border border-rose-400/50 hover:border-rose-300/70 text-white">
                 <span i-tabler-heart-filled class="animate-heartbeat"></span>
             </button>
             <NuxtLink to="https://github.com/DicSo92/SandboxScience" title="Github" aria-label="Github" target="_blank" flex items-center py-0 mr-2>
